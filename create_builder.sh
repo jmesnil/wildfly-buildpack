c5d1026b802e768c077d3261b11d37065ee365b2
Jeff Mesnil <jmesnil@redhat.com>
2021-06-02 14:48:46 +0200

    support REGISTRY env var to create the builder
    Signed-off-by: Jeff Mesnil <jmesnil@redhat.com>

--- create_builder.sh
+++ create_builder.sh
@@ -1,3 +1,3 @@
-docker build stacks/wildfly -t wildfly/wildfly-build:latest --target build
-docker build stacks/wildfly -t wildfly/wildfly-run:latest --target run
-pack builder create wildfly/wildfly-builder:latest --config ./builder.toml
+docker build stacks/wildfly -t ${REGISTRY}wildfly/wildfly-build:latest --target build
+docker build stacks/wildfly -t ${REGISTRY}wildfly/wildfly-run:latest --target run
+pack builder create ${REGISTRY}wildfly/wildfly-builder:latest --config ./builder.toml
