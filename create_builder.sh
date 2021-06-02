5f70513dc64cbac49dd0d3e9dae2d51bbd4ab09e
Jeff Mesnil <jmesnil@redhat.com>
2021-06-02 16:07:13 +0200

    fix cat
    Signed-off-by: Jeff Mesnil <jmesnil@redhat.com>

--- create_builder.sh
+++ create_builder.sh
@@ -1,11 +1,11 @@
-docker build stacks/wildfly -t ${REGISTRY}wildfly/wildfly-build:latest --target build
-docker build stacks/wildfly -t ${REGISTRY}wildfly/wildfly-run:latest --target run
+docker build stacks/wildfly -t ${REGISTRY}wildfly-build:latest --target build
+docker build stacks/wildfly -t ${REGISTRY}wildfly-run:latest --target run
 
 cat >> ./builder.toml <<EOL
 # This image is used at runtime
-run-image = "${REGISTRY}wildfly/wildfly-run:latest"
+run-image = "${REGISTRY}wildfly-run:latest"
 # This image is used at build-time
-build-image = "${REGISTRY}wildfly/wildfly-build:latest"
+build-image = "${REGISTRY}wildfly-build:latest"
 EOL
 
-pack builder create ${REGISTRY}wildfly/wildfly-builder:latest --config ./builder.toml
+pack builder create ${REGISTRY}wildfly-builder:latest --config ./builder.toml
