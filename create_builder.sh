76001379e40b576a1ea7702b63c24b772a6e39a9
Jeff Mesnil <jmesnil@redhat.com>
2021-06-02 14:56:26 +0200

    make build and run images configurable
    Signed-off-by: Jeff Mesnil <jmesnil@redhat.com>

--- create_builder.sh
+++ create_builder.sh
@@ -1,3 +1,11 @@
 docker build stacks/wildfly -t ${REGISTRY}wildfly/wildfly-build:latest --target build
 docker build stacks/wildfly -t ${REGISTRY}wildfly/wildfly-run:latest --target run
+
+cat ./builder.toml <<EOL
+# This image is used at runtime
+run-image = "${REGISTRY}wildfly/wildfly-run:latest"
+# This image is used at build-time
+build-image = "${REGISTRY}wildfly/wildfly-build:latest"
+EOL
+
 pack builder create ${REGISTRY}wildfly/wildfly-builder:latest --config ./builder.toml
